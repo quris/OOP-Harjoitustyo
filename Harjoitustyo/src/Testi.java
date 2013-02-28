b0c9558c0f0af06bca57a9f21ce864cf5fc73e6f
Axel <axjojo@utu.fi>
2013-02-28 11:50:10 +0200

    nollaus

--- Harjoitustyo/src/Testi.java
+++ Harjoitustyo/src/Testi.java
@@ -5,12 +5,6 @@ public class Testi {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String Janne = "Syöpä";
-		for(int i=0; i<9; i++){
-			System.out.println(Janne);
-		}
-		String Nexus = Janne;
-		System.out.println(Nexus);
 
 	}
 
